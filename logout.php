e46f6ea63b59d9c7eb62aef64792c3695c507595
Darien Ware <dware@artemis.cs.csub.edu>
2021-12-08 04:08:31 -0800

    created logout file

---
+++ logout.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> Index</title>
+    </head>
+    <body>
+
+    <?php
+    date_default_timezone_set('America/Los_Angeles');
+    error_reporting(E_ALL);
+    ini_set("log_errors", 1);
+    ini_set("display_errors", 1);
+
+    if (session_status() == PHP_SESSION_NONE)
+    {
+        session_start();
+    }
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+    
+    ?>
+    </body>
+</html>
