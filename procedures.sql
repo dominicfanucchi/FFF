aa5792a2ef4d131f4b28db8012ad914d9413bf88
Nofunzoner <nofunzone2@gmail.com>
2021-12-07 17:22:08 -0800

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -18,7 +18,7 @@ BEGIN
 	
 
 	IF @usercount > 0 THEN
-		SELECT NULL as UserID, "Username already exists" as 'Error';
+		SELECT "Username already exists" as 'Error';
 	ELSE
 		/*Just declaring variable. Setting to ID i know will be in table to prevent
 			accidental insertion */
@@ -28,7 +28,7 @@ BEGIN
 
 		INSERT INTO User(UserID, Username, Password, PhoneNumber, CheckStat)
 		VALUES (@uID, uName, pWord, phone, 'n');
-		SELECT Username AS username, NULL AS 'Error';
+		SELECT NULL AS 'Error';
 	END IF;
 
 END
@@ -52,7 +52,7 @@ BEGIN
 
 	ELSE
 		/* if user already exists, pull its uID */
-		SELECT UserID INTO @uID FROM User
+		SELECT `UserID` INTO @uID FROM User
 		WHERE User.Username = uName;
 
 	END IF;
@@ -63,16 +63,16 @@ BEGIN
 	/* If not a duplicate, add as employee */
 	IF @empcount < 1 THEN
 
-		SELECT Name INTO @ShelterName FROM Shelter
+		SELECT `Name` INTO @ShelterName FROM Shelter
 		WHERE Shelter.ShelterID = sID;
 
 		SELECT CURRENT_DATE() INTO @today;
 
 		INSERT INTO Employee(UserID, ShelterID, Name, HireDate)
 		VALUES (uID, sID, @ShelterName, today);
-		SELECT Username AS username, NULL AS 'Error';
+		SELECT NULL AS 'Error';
 	ELSE
-		SELECT NULL AS username, "Employee already exists";
+		SELECT "Employee already exists";
 
 	END IF;
 
@@ -83,7 +83,7 @@ END
 
 CREATE PROCEDURE `DeleteUser`(uName varchar(24))
 BEGIN
-	SELECT UserID INTO @uID FROM User
+	SELECT `UserID` INTO @uID FROM User
 	WHERE Username = uName;
 	SELECT COUNT(*) INTO @usercount FROM User
 	WHERE User.Username = uName;
@@ -108,14 +108,14 @@ END
 /* change to use username, not userid */
 CREATE PROCEDURE `AnimalAdoption`(uName varchar(24), aID int(11), price decimal(7,2))
 BEGIN 
-	SELECT UserID INTO @uID FROM User
+	SELECT `UserID` INTO @uID FROM User
 	WHERE Username = uName;
 
 	SELECT COUNT(*) INTO @usercount FROM User
 	WHERE User.UserID = @uID;
 	SELECT COUNT(*) INTO @animalcount FROM Animal
 	WHERE Animal.UserID = aID;
-	SELECT ShelterID INTO @sID FROM Animal
+	SELECT `ShelterID` INTO @sID FROM Animal
 	WHERE Animal.AnimalID = aID;
 	SELECT CURRENT_DATE() INTO @today;
 
@@ -134,8 +134,8 @@ BEGIN
 	/* just returns the number of adoptions by a user. Cant replace mid 
 	string with a variable in mariadb, so will need to do on php side */
 
-	SELECT UserID INTO @uID FROM User
-	WHERE Username = uName;
+	SELECT `UserID` INTO @uID FROM User
+	WHERE `Username` = uName;
 
 	SELECT COUNT(*) INTO @useradopts FROM Adopts
 	WHERE Adopts.UserID = @uID;
@@ -148,7 +148,7 @@ END
 
 CREATE PROCEDURE `GenerateID`(OUT uID int)
 BEGIN
-	SELECT UserID INTO uID FROM User
+	SELECT `UserID` INTO uID FROM User
 	WHERE User.Username = 'nofunzone';
 	
 	SELECT COUNT(*) INTO @exist FROM User
@@ -170,7 +170,7 @@ BEGIN
 	SET @fromDate = DATE_SUB(@today, INTERVAL 1 month);
 
 	SELECT * FROM Adopts
-	WHERE AdoptionDate >= @fromDate AND AdoptionDate <= @toDate;
+	WHERE `AdoptionDate` >= @fromDate AND AdoptionDate <= @toDate;
 
 
 END
