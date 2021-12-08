362b2a3ab8c0617bd9091040d98612e2fa3f6c10
Erica McKenna <emckenna@artemis.cs.csub.edu>
2021-12-08 06:13:55 -0800

    changes titles

--- usersplash.php
+++ usersplash.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
 	<link rel="stylesheet" type="text/css" href="index.css">
-	<title>Furry Friend Finder | Home</title>
+	<title>Furry Friend Finder | Adopter</title>
 </head>
 <style>
 	.dropbtn {
